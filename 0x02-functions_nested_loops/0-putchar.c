a4d05f35e4b389922a34455c000b898e31bcb6d4
SalisuAutaTuraki <dashekwe542@gmail.com>
2022-09-14 04:06:51 -0700

    Test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Print "main"
+ *
+ * Return: Always 0 (Success)
+ *
+ *     */ 
+int main(void)
+{
+	int i = 0;
+	char name_of_school[] = "main.h";
+
+	while (i < 9)
+	{
+		_putchar(name_of_school[i]);
+		i++;
+	}
+	_putchar('\n');
+	ret urn (0);
+}
+
