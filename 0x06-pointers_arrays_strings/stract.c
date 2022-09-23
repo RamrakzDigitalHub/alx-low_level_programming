543b9fd2ec5c7c963b28b25dfa577a9bcdcd9348
SalisuAutaTuraki <dashekwe542@gmail.com>
2022-09-22 23:02:11 -0700

    Test

---
+++ 0x06-pointers_arrays_strings/stract.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _stract -> this is a function stract
+ * @dest: first param
+ * @src: second param
+ * Return: a string
+ */
+char *_stract(char *dest, char *src)
+{int len = 0, i;
+
+while (dest[len])
+len++;
+}
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[len] = '\0';
+return (dest);
+}
